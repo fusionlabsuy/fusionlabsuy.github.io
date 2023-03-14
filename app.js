8cd345851cab50f55cb46186325e9e85ea474ec0
fusionlabs <123756617+fusionlabsuy@users.noreply.github.com>
2023-03-13 22:36:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,42 +14,91 @@ hamburger.onclick = function(){
 
     //display plan content:
 
+products = document.querySelector(".products");
+plans = document.querySelector(".plans");
+peek = document.querySelector(".peek");
+
 standard = document.querySelector(".standard");
 avanzado = document.querySelector(".avanzado");
 premium = document.querySelector(".premium");
 
-plans = document.querySelector(".plans");
-products = document.querySelector(".products");
+
+// Standard - toggle functions
 
 standard.onmouseover = function(){
     standard.classList.toggle("active");
+
     products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
 }
 
 standard.onmouseout = function(){
     standard.classList.toggle("active");
+
     products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
+}
+
+standard.onclick = function(){
+    standard.classList.toggle("active");
+
+    products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
 }
 
 
+// Avanzado - toggle functions
+
 avanzado.onmouseover = function(){
     avanzado.classList.toggle("active");
+
     products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
 }
 
 avanzado.onmouseout = function(){
     avanzado.classList.toggle("active");
+
     products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
+}
+
+avanzado.onclick = function(){
+    avanzado.classList.toggle("active");
+
+    products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
 }
 
 
+// Premium - toggle functions
+
 premium.onmouseover = function(){
     premium.classList.toggle("active");
+
     products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
 }
 
 premium.onmouseout = function(){
     premium.classList.toggle("active");
+
     products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
 }
 
+premium.onclick = function(){
+    premium.classList.toggle("active");
+
+    products.classList.toggle("active");
+    plans.classList.toggle("active");
+    peek.classList.toggle("active");
+}
