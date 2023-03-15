ed5daae6a2c8a78188f5d55c45d7db6cd22c447b
fusionlabs <123756617+fusionlabsuy@users.noreply.github.com>
2023-03-15 16:00:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,216 @@
-// FusionLabs - juanigomez | landing page - JavaScript
+<!-- FusionLabs - juanigomez | landing page - HTML -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FusionLabs - homepage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
 
-    //toggle responsive menu:
+        <!-- Navigation Bar -->
 
-    hamburger = document.querySelector(".hamburger");
-    navBar = document.querySelector(".nav-bar");
+        <header>
+            <div class="logo">
+                <img src="images/logo.s.w.png" alt="logo">
+                <h3>FusionLabs</h3>
+            </div>
+            <div class="hamburger">
+                <div>
+                    <span class="line-1"></span>
+                    <span class="line-2"></span>
+                    <span class="line-3"></span>
+                </div>
+            </div>
+            <nav class="nav-bar">
+                <ul>
+                    <li><a href="#products">Productos</a></li>
+                    <li><a href="#about-us">Sobre nosostros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#" class="btn-nav">Trabajos Recientes</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="navPos"></div>
+        
+        <div class="container">
+            <div class="left">
+                <div class="title">
+                    <h1>Vender online</h1>
+                    <h1>nunca fue tan facil</h1>
+                </div>
+                <p class="text">
+                    Hoy en dia todo se digitalizo, no pierdas la chance de realizar esta transicion con
+                    la ayuda de nuestro equipo altamente capacitado.  
+                </p>
+                <div>
+                    <a href="#" class="btn-wp">Get Started</a>
+                </div>
+                <div class="right"></div>
+            </div>
+        </div>   
+        
+        <!-- About Us -->
+        
+        <a id="about-us"></a>
+        <div class="selector">
+            <h1>Sobre nosotros...</h1>
+            <div class="top">
+                <div class="left">
+                    <img src="images/logo.u.b.png" alt="about us (1)">
+                </div>
+                <div class="right">
+                    <p>  
+                       Somos una empresa de software dedicada al diseno
+                       web. Nuestro objetivo es ayudar a todo tipo de 
+                       empresas a potenciar sus ventas mediante el uso
+                       de la tecnologia.
+                    </p>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="left">
+                    <p>
+                       Nuestro equipo de diseñadores y desarrolladores 
+                       trabaja con nuestros clientes para entender sus 
+                       necesidades y crear soluciones que se adaptan a 
+                       sus objetivos únicos.
+                    </p>
+                </div>
+                <div class="right">
+                    <img src="images/logo.u.b.png" alt="about us (2)">
+                </div>
+            </div>
+        </div>
+
+        <!-- Products: -->
+
+        <a id="products"></a>
+        <div class="products">
+            <h1>Nuestros Productos</h1>
+            <a href="#" class="btn-wp">Solicita tu Tienda online</a>
+            <div class="plans">
+                <div class="standard">
+                    <div class="peek">
+                        <h2>Standard</h2>
+                        <h3>USD 300</h3>
+                    </div>
+                    <div class="content">
+                        <div class="yes">
+                            <ul>
+                                <li>✅ Tienda online</li>
+                                <li>✅ Carga de Contenido</li>
+                                <li>✅ Sistema de pagos</li>
+                            </ul>
+                        </div>
+                        <div class="no">
+                            <ul>
+                                <li>❌ Diseno personalizado</li>
+                                <li>❌ Sistema complejo</li>
+                                <li>❌ Redes sociales</li>
+                                <li>❌ Velocidad optimizada</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h4>Entrega: 15 dias</h4>
+                </div>
+                <div class="avanzado">
+                    <div class="peek">
+                        <h2>Avanzado</h2>
+                        <h3>USD 500</h3>
+                    </div>
+                    <div class="content">
+                        <div class="yes">
+                            <ul>
+                                <li>✅ Tienda online</li>
+                                <li>✅ Carga de contenido</li>
+                                <li>✅ Sistema de pagos</li>
+                                <li>✅ Diseno personalizado</li>
+                            </ul>
+                        </div>
+                        <div class="no">
+                            <ul>
+                                <li>❌ Sistema complejo</li>
+                                <li>❌ Redes sociales</li>
+                                <li>❌ Velocidad optimizada</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h4>Entrega: 10 dias</h4>
+                </div>
+                <div class="premium">
+                    <div class="peek">
+                        <h2>Premium</h2>
+                        <h3>USD 1000</h3>
+                    </div>
+                    <div class="content">
+                        <div class="yes">
+                            <ul>
+                                <li>✅ Tienda online</li>
+                                <li>✅ Carga de contenido</li>
+                                <li>✅ Sistema de pagos</li>
+                                <li>✅ Diseno personalizado</li>
+                                <li>✅ Sistema complejo</li>
+                                <li>✅ Redes sociales</li>
+                                <li>✅ Velocidad optimizada</li>
+                            </ul>
+                        </div>
+                        <div class="no"></div>
+                    </div>
+                    <h4>Entrega: 7 dias</h4>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer: -->
+
+        <footer>
+            <div class="horizontal">
+                <div class="footer-logo">
+                    <div class="img-footer-fd">
+                        <img src="images/logo.u.b.png">
+                    </div>
+                    <div class="img-footer-rd">
+                        <img src="images/logo.s.b.png">
+                    </div>
+                </div>
+                <div class="pages-footer">
+                    <h4>Pages</h4>
+                    <ul>
+                        <li>Productos</li>
+                        <li>Sobre nosotros</li>
+                        <li>Contacto</li>
+                        <li>Trabajos recientes</li>
+                    </ul>
+                </div>
+                <div class="partners-footer">
+                    <h4>Partners</h4>
+                    <ul>
+                        <li>Shopify</li>
+                        <li>Bigcommerce</li>
+                        <li>Figma</li>
+                        <li>VSCode</li>
+                    </ul>
+                </div>
+                <div class="contact-footer">
+                    <h4>Contact</h4>
+                    <ul>
+                        <li>fusionlabsuy.githhub.io</li>
+                        <li>fusion.labs.uy@gmail.com</li>
+                        <li>+(598) 93 967 391</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+    </main>
+
+    <script src="app.js"></script>
     
-    hamburger.onclick = function(){
-        navBar.classList.toggle("active");
-        hamburger.classList.toggle("active");
-    }
-    
-    
-        //display plan content:
-    
-    products = document.querySelector(".products");
-    plans = document.querySelector(".plans");
-    
-    standard = document.querySelector(".standard");
-    avanzado = document.querySelector(".avanzado");
-    premium = document.querySelector(".premium");
-    
-    
-    // Standard - toggle functions
-    
-    standard.onmouseover = function(){
-        standard.classList.toggle("active");
-    
-        products.classList.toggle("active");
-        plans.classList.toggle("active");
-    }
-    
-    standard.onmouseout = function(){
-        standard.classList.toggle("active");
-    
-        products.classList.toggle("active");
-        plans.classList.toggle("active");
-    }
-    
-    
-    // Avanzado - toggle functions
-    
-    avanzado.onmouseover = function(){
-        avanzado.classList.toggle("active");
-    
-        products.classList.toggle("active");
-        plans.classList.toggle("active");
-    }
-    
-    avanzado.onmouseout = function(){
-        avanzado.classList.toggle("active");
-    
-        products.classList.toggle("active");
-        plans.classList.toggle("active");
-    }
-    
-    
-    // Premium - toggle functions
-    
-    premium.onmouseover = function(){
-        premium.classList.toggle("active");
-    
-        products.classList.toggle("active");
-        plans.classList.toggle("active");
-    }
-    
-    premium.onmouseout = function(){
-        premium.classList.toggle("active");
-    
-        products.classList.toggle("active");
-        plans.classList.toggle("active");
-    }
-    
-    
+</body>
+</html>
