240efc3874464db86de182b3c6d4347c19077e30
FusionLabs <123756617+fusionlabsuy@users.noreply.github.com>
2023-06-21 20:55:44 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -79,6 +79,27 @@ if (document.getElementById('landing-page')) {
   stepsHover(fourth);
 
 
+  // On Scroll effects
+
+  window.addEventListener('scroll', reveal);
+
+  function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
+
+      if(revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+      }else{
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
+
+
   // Send Email
 
   function wait(time){
