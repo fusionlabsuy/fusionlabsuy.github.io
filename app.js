0a43a1c88ef216761e86f2b5c048cd6c55c5c6aa
FusionLabs <123756617+fusionlabsuy@users.noreply.github.com>
2023-09-24 22:43:30 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -281,3 +281,142 @@ if (document.getElementById('index-page')) {
   }
 
 }
+
+
+
+// ChatBot
+
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+const categories = {
+    "help": ['Consulta', 'Duda', 'Pregunta'],
+    "thanks": ['Gracias', 'Util', 'Agradecido'],
+    "definitions": ['Landing', 'Hosting', 'E-commerce', 'Dominio', 'Software', 'FusionLabs', 'SSL', 'Programa', 'Algoritmo'],
+    "purpouse": ['Enfoque', 'Dedican', 'Hacen', 'Objetivo'],
+    "chatbot": ['Chatbot', 'Inteligencia Artificial', 'ia', 'ai', 'Persona', 'Verdad', 'Real']
+};
+
+const definitions = {
+  "landing": "Una landing page es una página web diseñada específicamente para una campaña de marketing o promoción. Su objetivo principal es convertir a los visitantes en clientes potenciales o clientes, generalmente a través de un formulario de registro o compra. Suelen ser páginas de destino únicas y no vinculadas a la navegación general del sitio web.",
+  "hosting": "El hosting, en el contexto de la web, se refiere al servicio que proporciona servidores y recursos necesarios para que un sitio web sea accesible en Internet. Los proveedores de hosting alojan los archivos y datos de un sitio web en servidores que están en línea las 24 horas del día, los 7 días de la semana, para que los usuarios puedan acceder a él en cualquier momento.",
+  "e-commerce": "El comercio electrónico, o e-commerce, es una forma de negocio en línea que implica la compra y venta de productos o servicios a través de Internet. Los sitios web de comercio electrónico permiten a los clientes explorar productos, agregar artículos a un carrito de compras, realizar pagos en línea y recibir productos o servicios en su ubicación deseada.",
+  "dominio": "Un dominio, en el contexto de Internet, es una dirección única que se utiliza para identificar un sitio web en línea. Por ejemplo, 'miempresa.com' es un dominio. Los usuarios pueden acceder a un sitio web escribiendo su dominio en un navegador web.",
+  "software": "El software se refiere a un conjunto de programas, datos y archivos diseñados para realizar tareas específicas en una computadora o dispositivo. Puede incluir aplicaciones, sistemas operativos, utilidades y más. El software permite a los usuarios interactuar con hardware y realizar diversas funciones en sus dispositivos.",
+  "fusionlabs": "FusionLabs es una empresa de tecnología especializada en el desarrollo de soluciones de software personalizadas para empresas. Ofrece servicios de consultoría y desarrollo de software a medida para satisfacer las necesidades específicas de sus clientes.",
+  "ssl": "SSL (Secure Sockets Layer) es un protocolo de seguridad que cifra la comunicación entre un navegador web y un servidor web. Se utiliza para proteger la confidencialidad de los datos transmitidos, como información de tarjetas de crédito y contraseñas, y garantizar que la conexión sea segura.",
+  "programa": "Un programa se refiere a un conjunto de instrucciones escritas en un lenguaje de programación que se utiliza para realizar una tarea específica en una computadora. Los programas pueden ser aplicaciones de software que los usuarios utilizan o componentes del sistema operativo.",
+  "algoritmo": "Un algoritmo es un conjunto ordenado de instrucciones diseñado para resolver un problema o realizar una tarea específica en un programa de computadora. Los algoritmos son la base de la programación y se utilizan para realizar cálculos, procesar datos y tomar decisiones.",
+};
+
+
+let userMessage = null; // Variable to store user's message
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+    // Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
+
+const generateResponse = (userMessage) => {
+    userMessage = userMessage.toLowerCase(); // Convert user message to lowercase for case-insensitive matching
+
+    // Check for categories
+    for (const category in categories) {
+        const keywords = categories[category];
+        if (checkKeywords(userMessage, keywords)) {
+            const detectedKeyword = getDetectedKeyword(userMessage, keywords);
+
+            if (category === "definitions") {
+                const definition = definitions[detectedKeyword.toLowerCase()];
+                if (definition) {
+                    const response = definition;
+                    return response;
+                }
+            } else if(category === "help") {
+              const response = `Dime, ¿cómo puedo ayudarte con tu ${detectedKeyword.toLowerCase()}?`;
+              return response;
+            } else if (category === "thanks"){
+              const response = `Me alegra haber sido util.`;
+              return response;
+            } else if (category === "purpouse") {
+              const response = 'Nuestro objetivo como empresa es proporcionar soluciones tecnológicas innovadoras y de alta calidad que ayuden a nuestros clientes a alcanzar sus metas y objetivos. Nos enfocamos en la excelencia y la satisfacción del cliente en cada proyecto que emprendemos.'
+              return response
+            } else if (category === "chatbot") {
+              const response = 'Soy un chatbot, no una persona real. Estoy diseñado para proporcionar información y asistencia en línea. Si tienes alguna pregunta o necesitas ayuda, ¡estaré encantado de ayudarte!'
+              return response
+            }
+        }
+    }
+
+    // If no specific category was found, provide a default response
+    const response = `Entiendo que dijiste: "${userMessage}". ¿En qué puedo ayudarte?`;
+    return response;
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+    if (!userMessage) return;
+
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        // Display "Thinking..." message while waiting for the response
+        const incomingChatLi = createChatLi("Pensando...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+
+        const response = generateResponse(userMessage);
+
+        setTimeout(() => {
+            // Replace the "Thinking..." message with the generated response
+            incomingChatLi.querySelector("p").textContent = response;
+            chatbox.scrollTo(0, chatbox.scrollHeight);
+        }, 1000);
+    }, 600);
+}
+
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
+function checkKeywords(userMessage, keywordArray) {
+    // Check if the user's message contains keywords from a specific category
+    return keywordArray.some(keyword => userMessage.includes(keyword.toLowerCase()));
+}
+
+function getDetectedKeyword(userMessage, keywordArray) {
+    // Get the detected keyword from a specific category
+    return keywordArray.find(keyword => userMessage.includes(keyword.toLowerCase()));
+}
+
